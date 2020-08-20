090a6716ebfa3082007bd84826bbf86f0e064542
Dean Illfeld <dean.illfeld@cmdsolutions.com.au>
2020-08-20 19:29:58 +1000

    fix bug renaming an instance when sg is created

--- security_groups.tf
+++ security_groups.tf
@@ -5,6 +5,10 @@ resource "aws_security_group" "main" {
   vpc_id = var.vpc_id
 
   tags = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group_rule" "ingress" {
